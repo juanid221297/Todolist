46fd189701db528a9b941ed8a66e8242adb3ecc6
juanid221297 <139110467+juanid221297@users.noreply.github.com>
2024-12-23 15:11:21 +0500

    Update build.sh

--- build.sh
+++ build.sh
@@ -3,6 +3,6 @@
 # Update and install OpenJDK
 apt-get update
 apt-get install -y openjdk-11-jdk
-
+java -version
 # Proceed with any other necessary setup, such as installing Python dependencies
 pip install -r requirements.txt
