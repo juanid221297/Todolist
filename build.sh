6c0832544f0dd040910d301d12eabda17b1fce0f
juanid221297 <139110467+juanid221297@users.noreply.github.com>
2024-12-23 16:50:56 +0500

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,11 +1,6 @@
 #!/bin/bash
 
 # Use the pre-installed Java (if available)
-export JAVA_HOME=/usr/lib/jvm/java-11-openjdk-amd64
-export PATH=$JAVA_HOME/bin:$PATH
-
-# Verify Java installation
-java -version || echo "Java not available; skipping Java setup."
-
+pip install git+https://github.com/PrithivirajDamodaran/Gramformer.git
 # Install Python dependencies
 pip install --no-cache-dir -r requirements.txt
