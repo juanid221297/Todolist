422d83e92da5f2c4f5fae0d74d0c593ba0aa4a3e
juanid221297 <139110467+juanid221297@users.noreply.github.com>
2024-12-23 15:20:35 +0500

    Update Grammar.py

--- Grammar.py
+++ Grammar.py
@@ -26,4 +26,4 @@ def check_grammar():
     return jsonify({"errors": errors})
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=6000, debug=True)
+    app.run(host="0.0.0.0", port=5000, debug=True)
