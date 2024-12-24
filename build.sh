e311ab0f5ab5407715906eecfe573d7590b0442d
juanid221297 <139110467+juanid221297@users.noreply.github.com>
2024-12-24 11:22:42 +0500

    Update build.sh

--- build.sh
+++ build.sh
@@ -4,3 +4,5 @@
 pip install git+https://github.com/PrithivirajDamodaran/Gramformer.git
 # Install Python dependencies
 pip install --no-cache-dir -r requirements.txt
+python -m spacy download en_core_web_sm
+
