c5b8c7e7ef10387451c95bae6b48695fee3a5efa
juanid221297 <139110467+juanid221297@users.noreply.github.com>
2024-12-25 16:28:53 +0500

    Update build.sh

--- build.sh
+++ build.sh
@@ -3,11 +3,8 @@
 # Exit immediately if any command fails
 set -e
 
-# Install all dependencies from requirements.txt (includes gunicorn if listed)
+# Install Python dependencies
 pip install --no-cache-dir -r requirements.txt
-
-# Install Gramformer directly from GitHub (if not listed in requirements.txt)
-pip install git+https://github.com/PrithivirajDamodaran/Gramformer.git
 
 # Install SpaCy language model
 python -m spacy download en_core_web_sm
