becdb50a276eaa1364572e07f071b91633be4816
juanid221297 <139110467+juanid221297@users.noreply.github.com>
2024-12-25 14:21:16 +0500

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,7 +1,3 @@
-#!/bin/bash
-
-# Install Gramformer directly from GitHub
+pip install gunicorn==20.1.0
 pip install git+https://github.com/PrithivirajDamodaran/Gramformer.git
-
-# Install SpaCy language model
 python -m spacy download en_core_web_sm
