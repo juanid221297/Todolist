b702f860eedeb3eadf9950c5db5d6b05e1b30501
juanid221297 <139110467+juanid221297@users.noreply.github.com>
2024-12-25 16:16:46 +0500

    Update Grammar.py

--- Grammar.py
+++ Grammar.py
@@ -1,13 +1,34 @@
 from flask import Flask, request, jsonify
 from gramformer import Gramformer
 from flask_cors import CORS
+from multiprocessing import active_children
+import atexit
 import os
+import signal
 
 app = Flask(__name__)  # Create the Flask app object
 CORS(app, resources={r"/*": {"origins": "*"}})  # Enable CORS for all routes
 
 # Load the Gramformer model globally during startup
 gf = Gramformer(models=1)  # Grammar correction model
+
+# Function to clean up all active child processes
+def cleanup_processes():
+    for process in active_children():
+        process.terminate()
+        process.join()
+
+# Register the cleanup function to be called during application exit
+atexit.register(cleanup_processes)
+
+# Ensure termination signals are handled
+def handle_exit_signal(signum, frame):
+    cleanup_processes()
+    os._exit(0)  # Exit the application
+
+# Set signal handlers
+signal.signal(signal.SIGTERM, handle_exit_signal)
+signal.signal(signal.SIGINT, handle_exit_signal)
 
 @app.route('/', methods=['POST'])
 def check_grammar():
