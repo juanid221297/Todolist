d59b810bc4105a7de5616691bb47a3e909c1f4ee
juanid221297 <139110467+juanid221297@users.noreply.github.com>
2024-12-26 12:43:31 +0500

    Update build.sh

--- build.sh
+++ build.sh
@@ -6,11 +6,13 @@ then
     echo "Docker could not be found, please install it."
     exit
 fi
+
+pip install -r requirements.txt
+
 poetry install
 
 # Build the Docker image
 docker build -t grammar-checker .
-pip install -r requirements.txt
 poetry install
 # Run the Docker container
 docker run -p 5000:5000 grammar-checker
